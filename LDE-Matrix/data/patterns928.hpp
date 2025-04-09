abf344fa6284c00acef30cb902d09350a0020761
Tim Cosgrove <chef.tim.cosgrove@gmail.com>
2025-04-09 18:39:13 -0400

    Saving valid / invalid patterns v1

--- LDE-Matrix/data/patterns928.hpp
+++ LDE-Matrix/data/patterns928.hpp
@@ -7,5 +7,6 @@
 extern std::map <int, std::string> PATTERNS_928;
 extern std::map <int, std::map <int, std::map <int, std::string>>> CASE_SUM_MAP_PATTERNS_928;
 extern std::map <int, std::map <int, bool>> CASE_PATTERN_MAP_PATTERNS_928;
+extern std::map <int, bool> PATTERN_928_VALIDITY;
 
 #endif // LDE_MATRIX_PATTERNS_928_HPP
